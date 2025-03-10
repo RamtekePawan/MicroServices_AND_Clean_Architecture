e696ee2913d137f94a778c32b3c0ec4c4e48dadf
Pawan Ramteke <131635249+RamtekePawan@users.noreply.github.com>
2025-03-10 10:31:42 +0530

    Delete catalog_service/dist directory

--- catalog_service/dist/api/rest_api/catalog.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post("/product", (req, res, next) => {
-    // Your handler logic here
-    res.status(201).json({ msg: "Product API" });
-});
-exports.default = router;
